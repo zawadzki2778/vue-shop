a44c11be2b65ddc405f1307802445062f48b8e91
zawadzki2778 <zawadzki2778@gmail.com>
2022-05-19 22:19:18 +0300

    l4/16m: created store.js and passing data through API

--- src/main.js
+++ src/main.js
@@ -3,3 +3,5 @@ import App from './App.vue'
 // import store from './vuex/store'
 
 createApp(App).mount('#app')
+
+
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,13 +1,41 @@
-// import Vue from 'vue'
-// import Vuex from 'vuex'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
 
-// Vue.use(Vuex);
+Vue.use(Vuex);
 
-// let store = new Vuex.Store (options, {
-//     state: {},
-//     mutations: {},
-//     actions: {},
-//     getters: {}
-// });
+let store = new Vuex.Store({
+  state: {
+    products: []
+  },
+  mutations: {
+    SET_PRODUCT_TO_STATE: (state, products) => {
+      state.products = products;
+    }
+  },
+  actions: {
+    GET_PRODUCTS_FROM_API({
+      commit
+    }) {
+      return axios('http://localhost:3000/products', {
+          method: "GET"
+        })
+        .then((products) => {
 
-// export default store;
+          commit('SET_PRODUCT_TO_STATE', products);
+          return products;
+        })
+        .catch((error) => {
+          console.log(error);
+          return error;
+        })
+    },
+    getters: {
+      PRODUCTS(state) {
+        return state.products;
+      }
+    }
+  }
+});
+
+export default store;
